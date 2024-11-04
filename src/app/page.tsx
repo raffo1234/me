d8fd07d287f3afc3f257ed16b7d9fdda457e9bc7
Rafael <rhmcord@gmail.com>
2024-11-04 18:15:01 -0500

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import { GiHummingbird } from "react-icons/gi";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center absolute left-0 right-0 top-0 bottom-0" style={{ backgroundColor: "#e27806" }}>
+  return (<main className="p-4">
+    <button className="block relative z-20 mx-auto p-6 border-dark border-solid border-2 transition-colors rounded-full hover:bg-dark hover:text-orange">
+        <GiHummingbird size={60} />
+      </button>
+    <section className="flex items-center z-10 justify-center absolute left-0 right-0 top-0 bottom-0 bg-orange">
       <div className="text-center">
       <h1 style={{fontSize: "144px"}}>
       RAFAEL MEZA
       </h1>
       <p style={{fontSize: "72px"}}>Senior Software Engineer</p>
       </div>
+    </section>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        dark: "#1a1a1a",
+        orange: "#e27806"
       },
     },
   },
