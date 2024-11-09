cb37cb4ddd63623b5064fe2bcf932a6c2fbb80d6
Rafael <rhmcord@gmail.com>
2024-11-09 17:07:54 -0500

    feat: add a new component called About
    - In order to have a clear code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import React, { Dispatch, ReactNode, SetStateAction } from "react";
 import { useState } from "react";
 import { FaDownload } from "react-icons/fa";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import About from "./components/about";
 
 interface Props {
   children?: ReactNode;
@@ -155,47 +156,7 @@ export default function Home() {
                   className="absolute left-0 top-0 w-1/2 h-1/2 rounded-tl-full"
                 ></button>
               </div>
-              <div
-                className={`${
-                  page === 1 ? "opacity-100 scale-100" : "opacity-0 scale-75"
-                } transition duration-500 ease-in-out flex overflow-hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray20 bg-opacity-50 w-[60%] h-[60%]`}
-              >
-                <div className="flex justify-end w-1/2 shrink-0">
-                  <p
-                    className={`${
-                      page === 1 ? "opacity-100" : "opacity-50"
-                    } transition text-right leading-[4.5rem] h-full items-center flex justify-end pr-6 tracking-wider text-[90px] font-druk mb-2 w-full shrink-0 bg-gray100 duration-500 ease-in-out delay-200 text-yellow10`}
-                  >
-                    RAFAEL <br />
-                    MEZA
-                  </p>
-                </div>
-                <div className="px-8 flex justify-center items-center">
-                  <div>
-                    <p
-                      className={`${
-                        page === 1
-                          ? "translate-y-0 opacity-100"
-                          : "translate-y-10 opacity-50"
-                      } transition mb-4 duration-500 ease-in-out delay-200 text-gray100`}
-                    >
-                      Known for delivering efficient, high-quality solutions and
-                      collaborating effectively with cross-functional teams.
-                    </p>
-                    <p
-                      className={`${
-                        page === 1
-                          ? "translate-y-0 opacity-100"
-                          : "translate-y-10 opacity-50"
-                      } transition duration-500 ease-in-out delay-200 text-gray100`}
-                    >
-                      Highly proficient in ReactJS, HTML5, CSS3, and adept at
-                      developing reusable code and optimizing UI/UX design
-                      feasibility.
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <About page={page} />
               <h2
                 onAnimationEnd={() => setEffect(false)}
                 className={`${
