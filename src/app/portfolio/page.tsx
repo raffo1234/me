19931fbf7e244b4fc191a7327f942586a18e7f99
Rafael <rhmcord@gmail.com>
2024-12-05 17:10:13 -0500

    feat: add project links

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import BulletList from "../components/bulletList";
 import ListStar from "../components/listStar";
-import { IoIosArrowRoundDown } from "react-icons/io";
+import { IoIosArrowRoundDown, IoIosGitNetwork } from "react-icons/io";
+import Link from "next/link";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 const PortfolioSection = ({
   imageSrc,
@@ -37,53 +39,80 @@ const projects = [
     title: "TIGER LATIN AMERICA",
     imageSrc: "/images/tiger.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/tiger/index.html",
   },
   {
     title: "Sunat Renta 2014",
     imageSrc: "/images/renta-2014.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/sunat_2015/index.html",
   },
   {
     title: "Golden Palace",
     imageSrc: "/images/goldenpalace.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/goldenpalace/index.html",
   },
   {
     title: "Laive",
     imageSrc: "/images/laive.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/laive/index.html",
   },
   {
     title: "A La Cancino",
     imageSrc: "/images/cancino.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/cancino/index.html",
   },
   {
     title: "Grupon",
     imageSrc: "/images/grupon.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/",
   },
   {
     title: "Travelmax",
     imageSrc: "/images/travelmax.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/_projects/travelmax/index.html",
   },
   {
     title: "Yachay",
     imageSrc: "/images/yachay.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/",
   },
   {
     title: "Dashboard Yachay",
     imageSrc: "/images/dashboard-yachay.png",
     features: ["Frontend Development", "HTML5, CSS3, JS, GSAP, JQUERY"],
+    href: "/",
   },
 ];
+
+const ProjectButton = ({ href, title }: { href: string; title: string }) => {
+  return (
+    <Link
+      style={{
+        backgroundImage: "linear-gradient(120deg, #FFD301 0%, #fda085 100%)",
+      }}
+      rel="noreferrer"
+      title={`${title} | Rafael Meza`}
+      href={href}
+      target="_blank"
+      className="transition-all mt-10 ring-opacity-30 focus:ring-4 duration-200 ring-0 ring-gray-300 hover:ring-8 px-8 py-5 text-[32px] space-x-5 items-center flex font-druk tracking-wide justify-center"
+    >
+      <RiExternalLinkLine size={28} color="white" />
+      <span className="underline">View Offline</span>
+    </Link>
+  );
+};
 
 export default function PortfolioPage() {
   return (
     <main className="pl-[350px] h-[100vh]">
-      {projects.map(({ title, imageSrc, features }, index) => {
+      {projects.map(({ title, href, imageSrc, features }, index) => {
         return (
           <PortfolioSection key={index} title={title} imageSrc={imageSrc}>
             <BulletList>
@@ -92,6 +121,7 @@ export default function PortfolioPage() {
                 return <ListStar key={index}>{feature}</ListStar>;
               })}
             </BulletList>
+            {href !== "/" ? <ProjectButton href={href} title={title} /> : null}
           </PortfolioSection>
         );
       })}
