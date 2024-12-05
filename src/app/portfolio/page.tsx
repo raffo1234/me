f7b1ce1f642568847f9325c8940620dcad544118
Rafael <rhmcord@gmail.com>
2024-12-05 14:47:45 -0500

    feat: apply reusable component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,166 +1,110 @@
+import { ReactNode } from "react";
 import BulletList from "../components/bulletList";
 import ListStar from "../components/listStar";
 import { IoIosArrowRoundDown } from "react-icons/io";
 
+const PortfolioSection = ({
+  imageSrc,
+  title,
+  children,
+}: {
+  imageSrc: string;
+  title: string;
+  children: ReactNode;
+}) => {
+  return (
+    <section className="relative mb-10 h-full flex justify-center">
+      <figure className="absolute right-0 top-1/2 -translate-y-1/2">
+        <img src={imageSrc} alt={title} />
+      </figure>
+      <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
+        <div>
+          <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
+            {title}
+          </h2>
+          {children}
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default function Portfolio() {
   return (
     <main className="pl-[350px] h-[100vh]">
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/tiger.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              TIGER LATIN AMERICA
-            </h2>
+      <PortfolioSection title="TIGER LATIN AMERICA" imageSrc="images/tiger.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection
+        title="Sunat Renta 2014"
+        imageSrc="images/renta-2014.png"
+      >
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection
+        title="Golden Palace"
+        imageSrc="images/goldenpalace.png"
+      >
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection title="Laive" imageSrc="images/laive.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection title="A La Cancino" imageSrc="images/cancino.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection title="Grupon" imageSrc="images/grupon.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection title="Travelmax" imageSrc="images/travelmax.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection title="Yachay" imageSrc="images/yachay.png">
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
+      <PortfolioSection
+        title="Dashboard Yachay"
+        imageSrc="images/dashboard-yachay.png"
+      >
+        <BulletList>
+          <h3 className="mb-3 font-bold">I am focused on:</h3>
+          <ListStar>Frontend Development</ListStar>
+          <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
+        </BulletList>
+      </PortfolioSection>
 
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/renta-2014.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Sunat Renta 2014
-            </h2>
-
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/goldenpalace.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Golden Palace
-            </h2>
-
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/laive.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Laive
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/cancino.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              A La Cancino
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/grupon.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Grupon
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/travelmax.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Travelmax
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/yachay.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Yachay
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
-      <section className="relative mb-10 h-full flex justify-center">
-        <figure className="absolute right-0 top-1/2 -translate-y-1/2">
-          <img src="images/dashboard-yachay.png" alt="Rafael Meza" />
-        </figure>
-        <div className="relative items-center max-w-[1024px] w-full h-full flex justify-start py-6">
-          <div>
-            <h2 className="max-w-[360px] uppercase mb-20 leading-[5rem] font-druk text-[100px] tracking-wider">
-              Dashboard Yachay
-            </h2>
-            <BulletList>
-              <h3 className="mb-3 font-bold">I am focused on:</h3>
-              <ListStar>Frontend Development</ListStar>
-              <ListStar>HTML5, CSS3, JS, GSAP, JQUERY</ListStar>
-            </BulletList>
-          </div>
-        </div>
-      </section>
       <div className="animate-bounce fixed bottom-10 left-1/2 -translate-x-1/2">
         <IoIosArrowRoundDown className="text-yellow10" size={80} />
       </div>
