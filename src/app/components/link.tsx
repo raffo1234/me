4e39f5cc0be4cd907b9f95bcdcd038f6d7c7d435
Rafael <rhmcord@gmail.com>
2024-12-07 13:55:43 -0500

    feat: add animation to links

--- src/app/components/link.tsx
+++ src/app/components/link.tsx
@@ -11,10 +11,10 @@ export default function UnderlinedLink({
       href={href}
       target="_blank"
       title={`Rafael Meza | ${title}`}
-      className="inline-block"
+      className="inline-block group"
     >
       <span className="block">{title}</span>
-      <span className="block w-full h-1 bg-yellow10"></span>
+      <span className="block w-full group-hover:w-0 transition-all translate-x-0 ease-in-out h-1 bg-yellow10"></span>
     </a>
   );
 }
